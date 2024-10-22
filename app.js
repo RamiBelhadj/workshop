fe46a27aa5786a3892b6b0fa5497aa380ee5bc7a
卄乇丨卄卂匚卄丨 <belhadjmohamedrami@gmail.com>
2024-10-22 21:54:43 +0200

    2snd day

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const mongoose = require("mongoose")
 require('dotenv').config()
 const bookRoutes = require("./routes/bookRoutes")
 const bodyParser = require("body-parser")
+const methodOverride = require("method-override")
 const app = express()
 const port = process.env.PORT || 8080
 
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(methodOverride('_method')) // PUT, DELETE methods
 // connection DB
 mongoose.connect(process.env.MONGO_URI)
 .then(()=> console.log("MongoDB connected"))
@@ -14,6 +17,11 @@ mongoose.connect(process.env.MONGO_URI)
 
 // def of routes
 app.use('/books', bookRoutes)
+
+// engine
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+app.set('views', 'views')
 
 // start the server
 app.listen(port, () => {
--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -7,13 +7,17 @@ const Book = require("../models/book")
 // get all books
 router.get('/', async (req, res)=>{
     try {
-        books = await Book.find()
-        res.json(books)
+        items = await Book.find()
+        //res.json(items)
+        res.render("books", {items})
     } catch (err) {
         res.status(500).json({message: err.message})
     }
 })
 
+router.get('/new', (req, res) => {
+    res.render("addbook")
+})
 // get a book by Id
 router.get('/:id', async (req, res)=>{
     try {
@@ -40,9 +44,37 @@ router.post('/', async (req, res)=>{
         res.status(500).json({message: err.message})
     }
 })
-router.put('/', (req, res)=>{
-    
-})
 
+router.put('/:id', async (req, res)=>{
+    try
+    {
+        const book = await Book.findById(new mongoose.Types.ObjectId(req.params.id))
+        if (book == null) {
+            return res.status(404).json({message: "Cannot find book"})
+        }
+        if (req.body.title != null) {
+            book.title = req.body.title
+        }
+        if (req.body.author != null) {
+            book.author = req.body.author
+        }
+        const updatedBook = await book.save()
+        res.status(201).json(updatedBook)
+    } catch (err) {
+        res.status(500).json({message: err.message})
+    }
+})
+router.delete('/:id', async (req, res) => {
+    try{
+        const book = await Book.findById(new mongoose.Types.ObjectId(req.params.id))
+        if (book == null) {
+            return res.status(404).json({message: "Cannot find book"})
+        }
+        await book.deleteOne()
+        res.json({message: "the book is deleted"})
+    } catch (err) { 
+        res.status(500).json({message: err.message})
+    }
+})
 
 module.exports = router;
